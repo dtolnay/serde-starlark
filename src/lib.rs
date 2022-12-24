bf2c87124c3d79a13888227e33f2d77a097bd82a
David Tolnay <dtolnay@gmail.com>
2022-12-23 19:27:39 -0800

    End serialized objects with trailing newline

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 mod error;
 mod ser;
 
+use crate::newline::WithNewline;
 use crate::ser::{WriteMap, WriteSeq, WriteStarlark};
 use serde::ser::{Impossible, Serialize};
 
@@ -20,95 +21,97 @@ pub struct Serializer;
 impl serde::Serializer for Serializer {
     type Ok = String;
     type Error = Error;
-    type SerializeSeq = WriteSeq<WriteStarlark>;
+    type SerializeSeq = WithNewline<WriteSeq<WriteStarlark>>;
     type SerializeTuple = Impossible<Self::Ok, Self::Error>;
     type SerializeTupleStruct = Impossible<Self::Ok, Self::Error>;
     type SerializeTupleVariant = Impossible<Self::Ok, Self::Error>;
-    type SerializeMap = WriteMap<WriteStarlark>;
+    type SerializeMap = WithNewline<WriteMap<WriteStarlark>>;
     type SerializeStruct = Impossible<Self::Ok, Self::Error>;
     type SerializeStructVariant = Impossible<Self::Ok, Self::Error>;
 
     fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_bool(v)
+        WriteStarlark::new().serialize_bool(v).map(newline)
     }
 
     fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_i8(v)
+        WriteStarlark::new().serialize_i8(v).map(newline)
     }
 
     fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_i16(v)
+        WriteStarlark::new().serialize_i16(v).map(newline)
     }
 
     fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_i32(v)
+        WriteStarlark::new().serialize_i32(v).map(newline)
     }
 
     fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_i64(v)
+        WriteStarlark::new().serialize_i64(v).map(newline)
     }
 
     fn serialize_i128(self, v: i128) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_i128(v)
+        WriteStarlark::new().serialize_i128(v).map(newline)
     }
 
     fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_u8(v)
+        WriteStarlark::new().serialize_u8(v).map(newline)
     }
 
     fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_u16(v)
+        WriteStarlark::new().serialize_u16(v).map(newline)
     }
 
     fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_u32(v)
+        WriteStarlark::new().serialize_u32(v).map(newline)
     }
 
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_u64(v)
+        WriteStarlark::new().serialize_u64(v).map(newline)
     }
 
     fn serialize_u128(self, v: u128) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_u128(v)
+        WriteStarlark::new().serialize_u128(v).map(newline)
     }
 
     fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_f32(v)
+        WriteStarlark::new().serialize_f32(v).map(newline)
     }
 
     fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_f64(v)
+        WriteStarlark::new().serialize_f64(v).map(newline)
     }
 
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_char(v)
+        WriteStarlark::new().serialize_char(v).map(newline)
     }
 
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_str(v)
+        WriteStarlark::new().serialize_str(v).map(newline)
     }
 
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_bytes(v)
+        WriteStarlark::new().serialize_bytes(v).map(newline)
     }
 
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_none()
+        WriteStarlark::new().serialize_none().map(newline)
     }
 
     fn serialize_some<T>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: Serialize + ?Sized,
     {
-        WriteStarlark::new().serialize_some(value)
+        WriteStarlark::new().serialize_some(value).map(newline)
     }
 
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_unit()
+        WriteStarlark::new().serialize_unit().map(newline)
     }
 
     fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_unit_struct(name)
+        WriteStarlark::new()
+            .serialize_unit_struct(name)
+            .map(newline)
     }
 
     fn serialize_unit_variant(
@@ -117,7 +120,9 @@ impl serde::Serializer for Serializer {
         variant_index: u32,
         variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        WriteStarlark::new().serialize_unit_variant(name, variant_index, variant)
+        WriteStarlark::new()
+            .serialize_unit_variant(name, variant_index, variant)
+            .map(newline)
     }
 
     fn serialize_newtype_struct<T>(
@@ -128,7 +133,9 @@ impl serde::Serializer for Serializer {
     where
         T: Serialize + ?Sized,
     {
-        WriteStarlark::new().serialize_newtype_struct(name, value)
+        WriteStarlark::new()
+            .serialize_newtype_struct(name, value)
+            .map(newline)
     }
 
     fn serialize_newtype_variant<T>(
@@ -141,11 +148,13 @@ impl serde::Serializer for Serializer {
     where
         T: Serialize + ?Sized,
     {
-        WriteStarlark::new().serialize_newtype_variant(name, variant_index, variant, value)
+        WriteStarlark::new()
+            .serialize_newtype_variant(name, variant_index, variant, value)
+            .map(newline)
     }
 
     fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        WriteStarlark::new().serialize_seq(len)
+        WriteStarlark::new().serialize_seq(len).map(WithNewline)
     }
 
     fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
@@ -171,7 +180,7 @@ impl serde::Serializer for Serializer {
     }
 
     fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
-        WriteStarlark::new().serialize_map(len)
+        WriteStarlark::new().serialize_map(len).map(WithNewline)
     }
 
     fn serialize_struct(
@@ -190,5 +199,62 @@ impl serde::Serializer for Serializer {
         len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
         WriteStarlark::new().serialize_struct_variant(name, variant_index, variant, len)
+    }
+}
+
+fn newline(mut starlark: String) -> String {
+    starlark.push('\n');
+    starlark
+}
+
+mod newline {
+    use super::newline;
+    use serde::ser::{Serialize, SerializeMap, SerializeSeq};
+
+    pub struct WithNewline<S>(pub(crate) S);
+
+    impl<S> SerializeSeq for WithNewline<S>
+    where
+        S: SerializeSeq<Ok = String>,
+    {
+        type Ok = S::Ok;
+        type Error = S::Error;
+
+        fn serialize_element<T>(&mut self, value: &T) -> Result<(), Self::Error>
+        where
+            T: Serialize + ?Sized,
+        {
+            self.0.serialize_element(value)
+        }
+
+        fn end(self) -> Result<Self::Ok, Self::Error> {
+            self.0.end().map(newline)
+        }
+    }
+
+    impl<S> SerializeMap for WithNewline<S>
+    where
+        S: SerializeMap<Ok = String>,
+    {
+        type Ok = S::Ok;
+        type Error = S::Error;
+
+        fn serialize_key<T>(&mut self, key: &T) -> Result<(), Self::Error>
+        where
+            T: Serialize + ?Sized,
+        {
+            self.0.serialize_key(key)
+        }
+
+        fn serialize_value<T>(&mut self, value: &T) -> Result<(), Self::Error>
+        where
+            T: Serialize + ?Sized,
+        {
+            self.0.serialize_value(value)
+        }
+
+        fn end(self) -> Result<Self::Ok, Self::Error> {
+            self.0.end().map(newline)
+        }
     }
 }
