196d2d679a508e7bb132fb97be6586f854a7b364
David Tolnay <dtolnay@gmail.com>
2022-12-26 20:19:35 -0800

    Add crosslink icons to top of rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! [![github]](https://github.com/dtolnay/serde-starlark)&ensp;[![crates-io]](https://crates.io/crates/serde_starlark)&ensp;[![docs-rs]](https://docs.rs/serde_starlark)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+//!
+//! <br>
+//!
 //! Serde serializer for generating syntactically valid Starlark, the
 //! declarative format used for describing build targets in build systems
 //! including [Bazel], [Buck], [Pants], and [Please].
