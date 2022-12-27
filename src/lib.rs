37a1617a2e7dc2686c2d26926ab78f278689c88b
David Tolnay <dtolnay@gmail.com>
2022-12-26 19:35:39 -0800

    Copy readme contents into crate-level rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,76 @@
+//! Serde serializer for generating syntactically valid Starlark, the
+//! declarative format used for describing build targets in build systems
+//! including [Bazel], [Buck], [Pants], and [Please].
+//!
+//! [Bazel]: https://bazel.build
+//! [Buck]: https://buck.build
+//! [Pants]: https://www.pantsbuild.org
+//! [Please]: https://please.build
+//!
+//! # Example
+//!
+//! The following example serializes a minimal Bazel target for the `syn` crate.
+//!
+//! The _tests/bazel.rs_ test in this repo has a somewhat more fleshed out
+//! example of this use case, including things like `load(…)`,
+//! `package(default_visibility = …)`, distinct `include` and `exclude`
+//! arguments to `glob(…)`, and `select({…})`.
+//!
+//! ```
+//! # use serde_derive::Serialize;
+//! # use std::collections::BTreeSet;
+//! #
+//! #[derive(Serialize)]
+//! #[serde(rename = "rust_library")]
+//! pub struct RustLibrary {
+//!     pub name: String,
+//!     pub srcs: Glob,
+//!     pub crate_features: BTreeSet<String>,
+//!     pub edition: u16,
+//!     pub deps: BTreeSet<String>,
+//! }
+//!
+//! #[derive(Serialize)]
+//! #[serde(rename = "glob")]
+//! pub struct Glob(pub BTreeSet<String>);
+//!
+//! fn main() {
+//! #   let rust_library = RustLibrary {
+//! #       name: "syn".to_owned(),
+//! #       srcs: Glob(BTreeSet::from_iter(["**/*.rs".to_owned()])),
+//! #       crate_features: BTreeSet::from_iter(["default".to_owned(), "full".to_owned()]),
+//! #       edition: 2018,
+//! #       deps: BTreeSet::from_iter([
+//! #           ":proc-macro2".to_owned(),
+//! #           ":quote".to_owned(),
+//! #           ":unicode-ident".to_owned(),
+//! #       ]),
+//! #   };
+//! #   let _ = stringify! {
+//!     let rust_library = RustLibrary { ... };
+//! #   };
+//!
+//!     print!("{}", serde_starlark::to_string(&rust_library).unwrap());
+//! }
+//! ```
+//!
+//! ```bzl
+//! rust_library(
+//!     name = "syn",
+//!     srcs = glob(["**/*.rs"]),
+//!     crate_features = [
+//!         "default",
+//!         "full",
+//!     ],
+//!     edition = 2018,
+//!     deps = [
+//!         ":proc-macro2",
+//!         ":quote",
+//!         ":unicode-ident",
+//!     ],
+//! )
+//! ```
+
 #![allow(
     clippy::enum_glob_use,
     clippy::missing_errors_doc,
