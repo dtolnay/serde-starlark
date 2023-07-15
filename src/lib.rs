38c38a4fd7d294422b4be4bccd9a697edddd1856
David Tolnay <dtolnay@gmail.com>
2023-07-15 14:09:22 -0700

    Release 0.1.14

--- src/lib.rs
+++ src/lib.rs
@@ -141,7 +141,7 @@
 //! present. Consider using `serde(skip_serializing_if = "Option::is_none")` to
 //! omit fields with value `None` from the serialized output.
 
-#![doc(html_root_url = "https://docs.rs/serde_starlark/0.1.13")]
+#![doc(html_root_url = "https://docs.rs/serde_starlark/0.1.14")]
 #![allow(
     clippy::doc_markdown,
     clippy::enum_glob_use,
