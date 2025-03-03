15c4152c4ab72b41ac9e9b5bdd1851d717663473
David Tolnay <dtolnay@gmail.com>
2025-03-02 18:26:47 -0800

    Ignore elidable_lifetime_names pedantic clippy lint
        warning: the following explicit lifetimes could be elided: 'a
         --> src/assignment.rs:4:6
          |
        4 | impl<'a, T> Serialize for Assignment<'a, T>
          |      ^^                              ^^
          |
          = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#elidable_lifetime_names
          = note: `-W clippy::elidable-lifetime-names` implied by `-W clippy::pedantic`
          = help: to override `-W clippy::pedantic` add `#[allow(clippy::elidable_lifetime_names)]`
        help: elide the lifetimes
          |
        4 - impl<'a, T> Serialize for Assignment<'a, T>
        4 + impl<T> Serialize for Assignment<'_, T>
          |
    
        warning: the following explicit lifetimes could be elided: 'a
         --> src/call.rs:7:6
          |
        7 | impl<'a, A> Serialize for FunctionCall<'a, A>
          |      ^^                                ^^
          |
          = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#elidable_lifetime_names
        help: elide the lifetimes
          |
        7 - impl<'a, A> Serialize for FunctionCall<'a, A>
        7 + impl<A> Serialize for FunctionCall<'_, A>
          |
    
        warning: the following explicit lifetimes could be elided: 'name
          --> src/call.rs:27:6
           |
        27 | impl<'name, S> FunctionCallSerializer<'name, S> {
           |      ^^^^^                            ^^^^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#elidable_lifetime_names
        help: elide the lifetimes
           |
        27 - impl<'name, S> FunctionCallSerializer<'name, S> {
        27 + impl<S> FunctionCallSerializer<'_, S> {
           |
    
        warning: the following explicit lifetimes could be elided: 'a
          --> src/call.rs:31:6
           |
        31 | impl<'a, S> Serializer for FunctionCallSerializer<'a, S>
           |      ^^                                           ^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#elidable_lifetime_names
        help: elide the lifetimes
           |
        31 - impl<'a, S> Serializer for FunctionCallSerializer<'a, S>
        31 + impl<S> Serializer for FunctionCallSerializer<'_, S>
           |
    
        warning: the following explicit lifetimes could be elided: 'a
         --> src/comment.rs:4:6
          |
        4 | impl<'a, T> Serialize for LineComment<'a, T>
          |      ^^                               ^^
          |
          = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#elidable_lifetime_names
        help: elide the lifetimes
          |
        4 - impl<'a, T> Serialize for LineComment<'a, T>
        4 + impl<T> Serialize for LineComment<'_, T>
          |

--- src/lib.rs
+++ src/lib.rs
@@ -144,6 +144,7 @@
 #![doc(html_root_url = "https://docs.rs/serde_starlark/0.1.16")]
 #![allow(
     clippy::doc_markdown,
+    clippy::elidable_lifetime_names,
     clippy::enum_glob_use,
     clippy::missing_errors_doc,
     clippy::missing_panics_doc,
